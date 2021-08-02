e079608b8458a874512f03dab4d7023040c2bc25
Bragaboll <bragaboll@hotmail.com>
2021-08-02 11:31:02 -0300

    Pagina Register

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -75,7 +75,7 @@ class _NovaSalaState extends State<Novasala> {
                   color: Colors.purple[50],
                 ),
                 margin: EdgeInsets.all(20),
-                width: 260,
+                width: 26git0,
                 height: 50,
                 child: Center(
                   child: Text(
