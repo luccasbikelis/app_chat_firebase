e0c8703e9c182216743bab18582237a504a24098
Bragaboll <bragaboll@hotmail.com>
2021-08-03 21:06:14 -0300

    Analitics Observer  na main, core 1.4.0 e Firebase Analitics 8.2.0 no Pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:app_chat_firebase/view/register.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
- 
+class _MyAppState extends State<MyApp> {
+  FirebaseAnalytics analytics = FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +22,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       home: NovaSala(),
     );
   }
