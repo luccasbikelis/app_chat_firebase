8ec4bb18790bb70f110a399a411b326cf2559f5c
Luccas Bikelis <luccasbikelis@gmail.com>
2021-08-09 17:01:40 -0300

    Atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-
 import 'package:app_chat_firebase/view/login.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
 import 'package:app_chat_firebase/view/register.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 
 void main() async {
@@ -16,7 +11,6 @@ void main() async {
   final FirebaseApp app = await Firebase.initializeApp();
   runApp(MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   @override
@@ -27,21 +21,16 @@ class _MyAppState extends State<MyApp> {
   FirebaseAnalytics analytics = FirebaseAnalytics();
 
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.purple,
-
       ),
       navigatorObservers: [
         FirebaseAnalyticsObserver(analytics: analytics),
       ],
-
-      home: NovaSala(),
-
+      home: Login(),
     );
   }
 }
